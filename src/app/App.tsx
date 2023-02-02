226ba173ac6c6e81ad31203988a95b274d243fc1
BadMonkeyDev <arsenii.aivazian@gmail.com>
2023-02-03 01:58:43 +0200

    updated packages

---
+++ src/app/App.tsx
@@ -0,0 +1,28 @@
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from "react-router-dom";
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
+
+const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
+    return (
+        <div className={classNames('app', {},[theme])}>
+            <button onClick={toggleTheme}>Change Theme</button>
+            <div>
+                <Link to={'/'}>Main</Link>
+                <Link to={'/about'}>About</Link>
+            </div>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
+                </Routes>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
