9d3eccd79017949a1b7d6ebc9e43515969498135
BadMonkeyDev <arsenii.aivazian@gmail.com>
2023-02-03 02:36:00 +0200

    updated packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import {render} from "react-dom";
 import App from "./app/App";
 import './app/styles/index.scss'
 import {BrowserRouter} from "react-router-dom";
 import {ThemeProvider} from "app/providers/ThemeProvider";
+import {createRoot} from "react-dom/client";
 
-render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const root = createRoot(rootElement);
+
+root.render(
     <BrowserRouter>
         <ThemeProvider>
             <App />
         </ThemeProvider>
-    </BrowserRouter>,
-    document.getElementById('root')
+    </BrowserRouter>
 )
